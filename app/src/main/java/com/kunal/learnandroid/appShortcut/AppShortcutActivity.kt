204e6ba08df529db028381c2bda57c95861067f5
Kunal Goyal <kunalgoyal000@gmail.com>
2024-03-23 18:58:59 +0530

    Added pinned shortcut support

--- app/src/main/java/com/kunal/learnandroid/appShortcut/AppShortcutActivity.kt
+++ app/src/main/java/com/kunal/learnandroid/appShortcut/AppShortcutActivity.kt
@@ -1,6 +1,11 @@
 package com.kunal.learnandroid.appShortcut
 
+import android.app.PendingIntent
 import android.content.Intent
+import android.content.pm.ShortcutInfo
+import android.content.pm.ShortcutManager
+import android.graphics.drawable.Icon
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,11 +14,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.core.content.getSystemService
 import androidx.core.content.pm.ShortcutInfoCompat
 import androidx.core.content.pm.ShortcutManagerCompat
 import androidx.core.graphics.drawable.IconCompat
@@ -47,6 +52,12 @@ class AppShortcutActivity: ComponentActivity() {
                     ) {
                      Text(text = "Add dynamic shortcut")
                     }
+
+                    Button(
+                        onClick = ::addPinnedShortcut
+                    ) {
+                        Text(text = "Add pinned shortcut")
+                    }
                 }
             }
         }
@@ -55,6 +66,40 @@ class AppShortcutActivity: ComponentActivity() {
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         handleIntent(intent)
+    }
+
+    private fun addPinnedShortcut() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return
+        }
+
+        val shortcutManager = getSystemService<ShortcutManager>()!!
+        if (shortcutManager.isRequestPinShortcutSupported) {
+            val shortcut = ShortcutInfo.Builder(applicationContext, "pinned")
+                .setShortLabel("Send message")
+                .setLongLabel("This will send a message to your friend")
+                .setIcon(
+                    Icon.createWithResource(
+                        applicationContext, R.drawable.baseline_3p_24
+                    )
+                )
+                .setIntent(
+                    Intent(applicationContext, AppShortcutActivity::class.java).apply {
+                        action = Intent.ACTION_VIEW
+                        putExtra("shortcut_id", "pinned")
+                    }
+                ).build()
+
+            val callbackIntent = shortcutManager.createShortcutResultIntent(shortcut)
+            val successPendingIntent = PendingIntent.getBroadcast(
+                applicationContext,
+                0,
+                callbackIntent,
+                PendingIntent.FLAG_IMMUTABLE
+            )
+            shortcutManager.requestPinShortcut(shortcut, successPendingIntent.intentSender)
+        }
+
     }
 
     private fun addDynamicShortcut() {
